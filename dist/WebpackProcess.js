e58f92c47beede0b9dc575b550e65a3f22897359
Florian Müller <florian.mueller@shopgate.com>
2017-11-09 17:44:31 +0100

    CON-2602 Added testing environment

---
+++ dist/WebpackProcess.js
@@ -0,0 +1,1 @@
+"use strict";Object.defineProperty(exports,"__esModule",{value:true});function _classCallCheck(instance,Constructor){if(!(instance instanceof Constructor)){throw new TypeError("Cannot call a class as a function")}}var WebpackProcess=function WebpackProcess(){_classCallCheck(this,WebpackProcess)};exports.default=new WebpackProcess;
--- src/WebpackProcess.js
+++ src/WebpackProcess.js
@@ -4,3 +4,12 @@
  * This source code is licensed under the Apache 2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
+
+/**
+ * The WebpackProcess class.
+ */
+class WebpackProcess {
+
+}
+
+export default new WebpackProcess();
